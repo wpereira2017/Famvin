eef5664014aeba052b8e5fd6c6a608b033cc060d
wpereira2017 <33041244+wpereira2017@users.noreply.github.com>
2020-11-24 14:46:37 -0600

    Agregar archivos de proyecto.

---
+++ Famvin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Famvin.Startup))]
+namespace Famvin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
